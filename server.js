452f2600c974ae440d85b0b86080e9f18fcb6f77
J-Young <jyoung222@my.bcit.ca>
2025-11-16 22:23:28 -0800

    complete public login

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import { configDotenv } from "dotenv";
+configDotenv();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Needed for __dirname when using ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve everything inside /public
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.get("/:page", (req, res) => {
+  const filePath = path.join(__dirname, "public", `${req.params.page}.html`);
+  res.sendFile(filePath, err => {
+    if (err) res.status(404).send("Page not found");
+  });
 });
+
+app.listen(3000, () => console.log(`Server running at ${process.env.IS_HOSTED}`));
